a9a12e1fff6abd71f68df6d2bf39a011e5621bbe
Tim Savage <tim@savage.company>
2016-10-28 11:22:03 +1100

    Added framework for setup, code climate, travis coverage tools etc

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyapp',
+    version='0.1',
+    description='Let us do the boilerplate',
+    long_description=open(path.join(here, 'README.rst'), encoding='utf-8').read(),
+    url='https://github.com/timsavage/pyapp',
+    author='Tim Savage',
+    author_email='tim@savage.company',
+    license='BSD',
+    keywords='application-framework',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    packages=find_packages(include=('pyapp*',)),
+    install_requires=[],
+)
