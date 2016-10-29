e162dd9ba0431c3fc2b590e82ebc07a821758ba9
Tim Savage <tim@savage.company>
2016-10-30 01:32:38 +1100

    Brought across a rought version of Django Checks

---
+++ pyapp/checks/registry.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import
+
+from pyapp.conf import settings
+from pyapp.utils import is_iterable
+
+from .messages import CheckMessage
+
+
+class CheckRegistry(object):
+    def __init__(self):
+        self.registered_checks = []
+
+    def register(self, check=None, *tags):
+        """
+        Can be used as a function or a decorator. Register given function
+        `func` labeled with given `tags`. The function should receive **kwargs
+        and return a list of Messages.
+        """
+        def inner(func):
+            func.tags = tags
+            if func not in self.registered_checks:
+                self.registered_checks.append(func)
+            return func
+
+        if callable(check):
+            return inner(check)
+        else:
+            if check:
+                tags += (check, )
+            return inner
+
+    def run_checks(self, tags=None):
+        """
+        Run all registered checks and return Messages.
+        """
+        messages = []
+        checks = list(self.registered_checks)
+
+        if tags:
+            # Filter the checks list
+            checks = [check for check in checks
+                      if hasattr(check, 'tags') and set(check.tags) & set(tags)]
+
+        for check in checks:
+            new_messages = check(settings=settings)
+            if isinstance(new_messages, CheckMessage):
+                messages.append(new_messages)
+            elif is_iterable(new_messages):
+                messages.extend(new_messages)
+
+        return messages
+
+
+# Singleton instance of registry
+registry = CheckRegistry()
+register = registry.register
+run_checks = registry.run_checks
---
+++ pyapp/utils.py
@@ -0,0 +1,13 @@
+def is_iterable(obj):
+    """
+    Determine if an object is iterable.
+
+    :rtype: bool
+
+    """
+    try:
+        iter(obj)
+    except TypeError:
+        return False
+    else:
+        return True
