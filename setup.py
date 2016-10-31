04f3ae3ced39e3df5061b4beb7d186d09a8145db
Tim Savage <tim@savage.company>
2016-11-01 10:47:05 +1100

    Tweak to project name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='pyapp',
+    name='pyApp',
     version='0.1',
     description='A Python application framework - Let us handle the boring stuff!',
     long_description=open(path.join(here, 'README.rst'), encoding='utf-8').read(),
@@ -33,7 +33,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
