4e305928544786ab7786dcee10e1e81becdc777d
Tim Savage <tim@savage.company>
2016-11-01 10:46:53 +1100

    Tests for CheckRegistry.register

--- tests/test_checks_registry.py
+++ tests/test_checks_registry.py
@@ -4,4 +4,36 @@ from pyapp.checks import registry
 
 
 class TestCheckRegistry(object):
-    pass
+    def test_register__with_decorator_without_tags(self):
+        target = registry.CheckRegistry()
+
+        @target.register
+        def my_check_func(**kwargs):
+            pass
+
+        assert len(target.registered_checks) == 1
+        assert my_check_func in target.registered_checks
+        assert len(my_check_func.tags) == 0
+
+    def test_register__with_decorator_with_tags(self):
+        target = registry.CheckRegistry()
+
+        @target.register("foo", "bar")
+        def my_check_func(**kwargs):
+            pass
+
+        assert len(target.registered_checks) == 1
+        assert my_check_func in target.registered_checks
+        assert len(my_check_func.tags) == 2
+
+    def test_register__with_method(self):
+        target = registry.CheckRegistry()
+
+        def my_check_func(**kwargs):
+            pass
+
+        target.register(my_check_func, "foo", "bar")
+
+        assert len(target.registered_checks) == 1
+        assert my_check_func in target.registered_checks
+        assert len(my_check_func.tags) == 2
