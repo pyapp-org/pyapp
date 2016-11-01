5f0ce0a09bc0450f089c9be3afe46d525d7e0edf
Tim Savage <tim@savage.company>
2016-11-01 19:50:58 +1100

    Added check for DEBUG setting

---
+++ tests/test_checks_built_in_security.py
@@ -0,0 +1,19 @@
+from pyapp.conf import settings
+
+from pyapp.checks.built_in import security
+
+
+class TestSecurityChecks(object):
+    def test_debug_enabled__check_defaults(self):
+        result = security.debug_enabled(settings)
+
+        # The default is for DEBUG to be False
+        assert result is None
+
+    def test_debug_enabled__warning_returned_if_enabled(self):
+        with settings.modify() as patch:
+            patch.DEBUG = True
+
+            result = security.debug_enabled(settings)
+
+        assert result == security.W001
