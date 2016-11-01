fa8a5c5872d782393a84c59da7207aaf7eca1d48
Tim Savage <tim@savage.company>
2016-11-01 18:33:55 +1100

    Migrated to using OpenStack pbr for building packages

--- setup.py
+++ setup.py
@@ -1,44 +1,7 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+#!/usr/bin/env python
+from setuptools import setup
 
 setup(
-    name='pyApp',
-    version='0.1',
-    description='A Python application framework - Let us handle the boring stuff!',
-    long_description=open(path.join(here, 'README.rst'), encoding='utf-8').read(),
-    url='https://github.com/timsavage/pyapp',
-    author='Tim Savage',
-    author_email='tim@savage.company',
-    license='BSD',
-    keywords='application-framework',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-
-        'Intended Audience :: Developers',
-
-        'License :: OSI Approved :: BSD License',
-
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-
-    packages=find_packages(include=('pyapp*',)),
-    install_requires=[],
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
