553d4e11a5e0f4360f5ba99a262afea7d77ce9f1
Tim Savage <tim@savage.company>
2016-11-01 18:02:14 +1100

    More tests for registry

--- tests/test_checks_registry.py
+++ tests/test_checks_registry.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pyapp.checks import registry
+from pyapp.checks import messages
 
 
 class TestCheckRegistry(object):
@@ -37,3 +38,87 @@ class TestCheckRegistry(object):
         assert len(target.registered_checks) == 1
         assert my_check_func in target.registered_checks
         assert len(my_check_func.tags) == 2
+
+    def test_register__same_check(self):
+        target = registry.CheckRegistry()
+
+        @target.register
+        def my_check_func(**kwargs):
+            pass
+
+        target.register(my_check_func, "foo", "bar")
+
+        assert len(target.registered_checks) == 1
+        assert my_check_func in target.registered_checks
+        assert len(my_check_func.tags) == 2
+
+    def test_run_checks__all_successful(self):
+        target = registry.CheckRegistry()
+
+        @target.register
+        def check_1(settings, **kwargs):
+            assert settings
+
+        @target.register
+        def check_2(settings, **kwargs):
+            assert settings
+
+        actual = target.run_checks()
+
+        assert len(actual) == 0
+
+    def test_run_checks__singular_and_multiple_responses(self):
+        target = registry.CheckRegistry()
+
+        @target.register
+        def check_1(**kwargs):
+            return messages.Info("Message1")
+
+        @target.register
+        def check_2(**kwargs):
+            return messages.Info("Message2"), messages.Info("Message3")
+
+        actual = target.run_checks()
+
+        assert len(actual) == 3
+        assert [
+            messages.Info("Message1"),
+            messages.Info("Message2"),
+            messages.Info("Message3"),
+        ] == actual
+
+    def test_run_checks__filter_by_tag(self):
+        target = registry.CheckRegistry()
+
+        @target.register('foo')
+        def check_1(**kwargs):
+            return messages.Info("Message1")
+
+        @target.register('foo', 'bar')
+        def check_2(**kwargs):
+            return messages.Info("Message2")
+
+        @target.register('bar')
+        def check_3(**kwargs):
+            return messages.Info("Message3"), messages.Info("Message4")
+
+        actual = target.run_checks(['foo'])
+        assert [
+            messages.Info("Message1"),
+            messages.Info("Message2"),
+        ] == actual
+
+        actual = target.run_checks(['bar'])
+        assert [
+            messages.Info("Message2"),
+            messages.Info("Message3"),
+            messages.Info("Message4"),
+        ] == actual
+
+        actual = target.run_checks(['foo', 'bar'])
+        assert [
+            messages.Info("Message1"),
+            messages.Info("Message2"),
+            messages.Info("Message3"),
+            messages.Info("Message4"),
+        ] == actual
