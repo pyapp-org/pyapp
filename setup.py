41ae0aafcd3e13f5894f76984dfebbbe07abfebb
Tim Savage <tim@savage.company>
2016-11-20 22:16:29 +1100

    Simplification of setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
 from setuptools import setup
 
-setup(
-    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
-    pbr=True,
-)
+setup(setup_requires=['pbr>=1.9', 'setuptools>=17.1'], pbr=True)
