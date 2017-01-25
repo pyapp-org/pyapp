14acc24d7d6a87df059b5d64ab5c99348a6d4296
Tim Savage <tim@savage.company>
2017-01-25 12:04:39 +1100

    Re-ordered params (--nocolor is now a top level argument)

--- tests/test_checks_report.py
+++ tests/test_checks_report.py
@@ -8,8 +8,8 @@ from tests.sample_app.__main__ import app
 @pytest.mark.parametrize('args', (
     ('checks',),
     ('checks', '--verbose'),
-    ('checks', '--nocolor'),
-    ('checks', '--verbose', '--nocolor'),
+    ('--nocolor', 'checks'),
+    ('--nocolor', 'checks', '--verbose'),
 ))
 def test_run_report_from_app(args, exit_code=4):
     with settings.modify() as patch:
