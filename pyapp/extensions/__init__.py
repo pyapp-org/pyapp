7eca4040b162dee847c4a59bfa9fd605699a60a3
Tim Savage <tim@savage.company>
2017-02-08 16:10:03 +1100

    Corrected naming "that could have sucked!"

---
+++ pyapp/extensions/__init__.py
@@ -0,0 +1,12 @@
+"""
+Extensions
+~~~~~~~~~~
+
+*Provides extensions to PyApp that allows other modules to register checks and default settings.*
+
+An extension
+
+"""
+from __future__ import absolute_import
+
+from .registry import *
--- pyapp/extensions/report.py
+++ pyapp/extensions/report.py
@@ -2,7 +2,7 @@ from __future__ import print_function, unicode_literals
 
 import sys
 
-from pyapp.extentions.registry import registry
+from pyapp.extensions.registry import registry
 
 try:
     import colorama
