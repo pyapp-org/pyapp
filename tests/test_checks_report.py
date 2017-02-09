644129184e2680cf34ebb4d36d1ae3baaccf16e7
Tim Savage <tim@savage.company>
2017-02-09 12:15:29 +1100

    Trigger checks prior to other command

--- tests/test_checks_report.py
+++ tests/test_checks_report.py
@@ -10,6 +10,7 @@ from tests.sample_app.__main__ import app
     ('checks', '--verbose'),
     ('--nocolor', 'checks'),
     ('--nocolor', 'checks', '--verbose'),
+    ('--checks', 'extensions', '--verbose'),
 ))
 def test_run_report_from_app(args, exit_code=4):
     with settings.modify() as patch:
