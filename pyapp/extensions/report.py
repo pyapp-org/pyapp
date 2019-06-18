ad03529f9ab23b688e692927a650de250294cfd0
Tim Savage <tim@savage.company>
2019-06-19 09:45:08 +1000

    Reformatted to extensions report to use fstrings

--- pyapp/extensions/report.py
+++ pyapp/extensions/report.py
@@ -12,6 +12,27 @@ class ExtensionReport:
 
     width = 80
 
+    BASIC_TEMPLATE_MONO = "+ {name} ({version})\n"
+    VERBOSE_TEMPLATE_MONO = (
+        f"{'=' * width}\n"
+        f" Name:       {{name}}\n"
+        f" Version     {{version}}\n"
+        f" Settings:   {{default_settings}}\n"
+        f" Has Checks: {{has_checks}}\n"
+        f"{'=' * width}\n\n"
+    )
+    BASIC_TEMPLATE = (
+        f"{Fore.YELLOW}+{Fore.CYAN} {{name}}{Style.RESET_ALL} ({{version}})\n"
+    )
+    VERBOSE_TEMPLATE = (
+        f"{Fore.YELLOW}{'=' * width}{Style.RESET_ALL}\n"
+        f"{Style.BRIGHT} Name:       {Style.RESET_ALL}{Fore.CYAN}{{name}}{Style.RESET_ALL}\n"
+        f"{Style.BRIGHT} Version:    {Style.RESET_ALL}{Fore.CYAN}{{version}}{Style.RESET_ALL}\n"
+        f"{Style.BRIGHT} Settings:   {Style.RESET_ALL}{Fore.CYAN}{{default_settings}}{Style.RESET_ALL}\n"
+        f"{Style.BRIGHT} Has Checks: {Style.RESET_ALL}{Fore.CYAN}{{has_checks}}{Style.RESET_ALL}\n"
+        f"{Fore.YELLOW}{'=' * width}{Style.RESET_ALL}\n\n"
+    )
+
     def __init__(
         self,
         verbose=False,
@@ -35,53 +56,11 @@ class ExtensionReport:
 
         # Generate templates
         if self.no_color:
-            self.BASIC_TEMPLATE = "+ {name} ({version})\n"
-            self.VERBOSE_TEMPLATE = (
-                ("=" * self.width)
-                + "\n Name:       {name}"
-                + "\n Version:    {version}"
-                + "\n Settings:   {default_settings}"
-                + "\n Has Checks: {has_checks}\n"
-                + ("=" * self.width)
-                + "\n\n"
-            )
-
+            self.basic_template = self.BASIC_TEMPLATE_MONO
+            self.verbose_template = self.VERBOSE_TEMPLATE_MONO
         else:
-            self.BASIC_TEMPLATE = (
-                Fore.YELLOW
-                + "+"
-                + Fore.CYAN
-                + " {name}"
-                + Style.RESET_ALL
-                + " ({version})\n"
-            )
-            self.VERBOSE_TEMPLATE = (
-                Fore.YELLOW
-                + ("=" * self.width)
-                + Style.RESET_ALL
-                + Style.BRIGHT
-                + "\n Name:       "
-                + Style.RESET_ALL
-                + Fore.CYAN
-                + "{name}"
-                + Style.RESET_ALL
-                + Style.BRIGHT
-                + "\n Version:    "
-                + Style.RESET_ALL
-                + "{version}"
-                + Style.BRIGHT
-                + "\n Settings:   "
-                + Style.RESET_ALL
-                + "{default_settings}"
-                + Style.BRIGHT
-                + "\n Has Checks: "
-                + Style.RESET_ALL
-                + "{has_checks}\n"
-                + Fore.YELLOW
-                + ("=" * self.width)
-                + Style.RESET_ALL
-                + "\n\n"
-            )
+            self.basic_template = self.BASIC_TEMPLATE
+            self.verbose_template = self.VERBOSE_TEMPLATE
 
     def output_result(self, extension: ExtensionDetail):
         """
@@ -95,9 +74,9 @@ class ExtensionReport:
         )
 
         if self.verbose:
-            self.f_out.write(self.VERBOSE_TEMPLATE.format(**format_args))
+            self.f_out.write(self.verbose_template.format(**format_args))
         else:
-            self.f_out.write(self.BASIC_TEMPLATE.format(**format_args))
+            self.f_out.write(self.basic_template.format(**format_args))
 
     def run(self):
         """
