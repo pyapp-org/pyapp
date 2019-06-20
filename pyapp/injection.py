62d9cca9993f54185db820f8c6a7f5655681c81d
Tim Savage <tim@savage.company>
2019-06-20 15:50:43 +1000

    Added basic IoC (Inversion of Control) dependency injection

--- pyapp/injection.py
+++ pyapp/injection.py
@@ -6,66 +6,128 @@ Support for dependency injection from factories.
 
 """
 
-from typing import TypeVar, Generic, Callable, Sequence, Tuple
+import abc
+import functools
+import inspect
 
-IT_co = TypeVar("IT_co", covariant=True)
+from types import FunctionType
+from typing import Callable, Dict, TypeVar, Optional
 
+__all__ = ("FactoryRegistry", "default_registry", "register_factory", "inject")
 
-class Inject(Generic[IT_co]):
-    def __init__(self, *factory_args, **factory_kwargs):
-        self.factory_args = factory_args
-        self.factory_kwargs = factory_kwargs
-
-
-class InjectionWrapper:
-    def __init__(self, func: Callable, dependencies: Sequence[Tuple[str, Callable]]):
-        self.func = func
-        self.dependencies = dependencies
-
-    def __call__(self, *args, **kwargs):
-        for name, factory in self.dependencies:
-            kwargs.setdefault(name, factory())
-        return self.func(self, *args, **kwargs)
+AT_co = TypeVar("AT_co", bound=abc.ABCMeta, covariant=True)
 
 
-class Dependencies:
-    def __init__(self, type_or_class: type):
-        self.type_or_class = type_or_class
-        self._dependencies()
+class InjectionError(Exception):
+    """
+    Error that is raised if a value cannot be injected.
 
-    def __call__(self, *args, **kwargs):
-        return self.type_or_class(*args, **kwargs)
-
-    def _dependencies(self):
-        functions = {
-            n: f
-            for n, f in self.type_or_class.__dict__.items()
-            if isinstance(f, Callable)
-        }
-
-        for name, func in functions.items():
-            dependencies = []
-            for arg, annotation in func.__annotations__.items():
-                if isinstance(annotation, Inject):
-                    expected_type = annotation.__orig_class__.__args__[0]
-                    dependencies.append((arg, lambda: "eek"))
-
-            if dependencies:
-                setattr(self.type_or_class, name, InjectionWrapper(func, dependencies))
+    This is to distinguish from a factory method raising an exception other than
+    from injection.
+    """
 
 
-@Dependencies
-class MyObject:
-    def __init__(self):
-        self.arg = None
-
-    def __str__(self):
-        return str(self.arg)
-
-    def call_me(self, arg: Inject[str]("default") = None):
-        self.arg = arg
+class InjectionSetupError(Exception):
+    """
+    Error generated during the setup of injection.
+    """
 
 
-obj = MyObject()
-obj.call_me()
-print(obj)
+class FactoryRegistry(Dict[abc.ABCMeta, Callable]):
+    """
+    Registry of type factories.
+    """
+
+    def register_factory(self, abstract_type: AT_co, factory: Callable):
+        """
+        Register a factory method for providing a abstract type.
+
+        :param abstract_type: A type based on `abc.ABC` or using `abc.ABCMeta`
+        :param factory: A factory that generates concrete instances based off the abstract type.
+
+        """
+        if not isinstance(abstract_type, abc.ABCMeta):
+            raise TypeError("'abstract_type' must utilise ABCMeta")
+        self[abstract_type] = factory
+
+    def resolve_factory(self, abstract_type: AT_co) -> Optional[Callable[[], AT_co]]:
+        """
+        Resolve an abstract type to a factory.
+        """
+        return self.get(abstract_type)
+
+
+# Define the global default registry
+default_registry = FactoryRegistry()
+register_factory = default_registry.register_factory
+
+
+class FactoryArgs:
+    __slots__ = ("args", "kwargs")
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+def _get_factory(parameter: inspect.Parameter, registry: FactoryRegistry):
+    """
+    Determine if a parameter can be injected.
+    """
+    default = parameter.default
+    if isinstance(default, FactoryArgs):
+        if parameter.kind is not parameter.KEYWORD_ONLY:
+            raise InjectionSetupError("Only keyword-only arguments can be injected.")
+
+        factory = registry.resolve_factory(parameter.annotation)
+        if not factory:
+            raise InjectionSetupError("No type specified with factory args")
+
+        return functools.partial(factory, *default.args, **default.kwargs)
+
+    # Ensure that the annotation is an ABC.
+    if isinstance(parameter.annotation, abc.ABCMeta):
+        return registry.resolve_factory(parameter.annotation)
+
+
+def _build_dependencies(func: FunctionType, registry: FactoryRegistry):
+    """
+    Build a list of dependency objects.
+    """
+    sig = inspect.signature(func)
+
+    dependencies = []
+    for name, parameter in sig.parameters.items():
+        factory = _get_factory(parameter, registry)
+        if factory:
+            dependencies.append((name, factory))
+
+    return tuple(dependencies)
+
+
+def inject(func: FunctionType = None, *, registry: FactoryRegistry = None):
+    """
+    Mark a function to have arguments injected.
+
+    A specific registry can be provided, else the global registry is used.
+    """
+    if func is None:
+        return lambda f: inject(f, registry=registry)
+
+    dependencies = _build_dependencies(func, registry or default_registry)
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Apply dependencies
+        for name, factory in dependencies:
+            try:
+                kwargs[name] = factory()
+            except Exception as ex:
+                raise InjectionError(
+                    f"Unable to instantiate instance for {name}."
+                ) from ex
+
+        return func(*args, **kwargs)
+
+    # If no dependencies are found just return original function
+    return wrapper if dependencies else func
---
+++ tests/test_injection.py
@@ -0,0 +1,55 @@
+import abc
+import pytest
+
+from pyapp import injection
+
+
+class ThingBase(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def do_stuff(self, item: str):
+        pass
+
+
+class AThing(ThingBase):
+    def do_stuff(self, item: str):
+        print(f"Doing A to {item}")
+
+
+class BThing(ThingBase):
+    def do_stuff(self, item: str):
+        print(f"Doing B to {item}")
+
+
+def thing_factory(name: str = None) -> ThingBase:
+    name = name or "default"
+    return {"default": AThing, "b": BThing}[name]()
+
+
+local_registry = injection.FactoryRegistry()
+local_registry.register_factory(ThingBase, thing_factory)
+
+
+def test_default_injection():
+    actual = None
+
+    @injection.inject(registry=local_registry)
+    def get_value(value: ThingBase):
+        nonlocal actual
+        actual = value
+
+    get_value()
+
+    assert isinstance(actual, AThing)
+
+
+def test_injection_args():
+    actual = None
+
+    @injection.inject(registry=local_registry)
+    def get_value(*, value: ThingBase = injection.FactoryArgs("b")):
+        nonlocal actual
+        actual = value
+
+    get_value()
+
+    assert isinstance(actual, BThing)
