94bc6a53bb7f7868e4f2837345441b2077de809f
Tim Savage <tim@savage.company>
2019-07-19 17:26:21 +1000

    Stored version in __version__.py

--- pyapp/__init__.py
+++ pyapp/__init__.py
@@ -8,10 +8,10 @@ PyApp - A python application framework
 """
 import logging
 
+from .__version__ import __version__
 from .versioning import get_installed_version
 
 # Configure a default null handler for logging.
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Tim Savage <tim@savage.company>"
-__version__ = get_installed_version("pyApp", __file__)
