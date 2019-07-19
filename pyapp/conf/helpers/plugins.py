3f02311d01571752714d5e154c1d536924b25d5f
Tim Savage <tim@savage.company>
2019-07-19 21:36:12 +1000

    Catch AttributeError as this can affect importing

--- pyapp/conf/helpers/plugins.py
+++ pyapp/conf/helpers/plugins.py
@@ -33,7 +33,13 @@ from typing import TypeVar, Type
 
 from pyapp import checks
 from pyapp.conf import settings
-from pyapp.exceptions import InvalidSubType, NotProvided, NotFound, BadAlias, CannotImport
+from pyapp.exceptions import (
+    InvalidSubType,
+    NotProvided,
+    NotFound,
+    BadAlias,
+    CannotImport,
+)
 from pyapp.utils import cached_property, import_type
 from .bases import (
     DefaultCache,
@@ -146,7 +152,9 @@ class NamedPluginFactory(FactoryMixin[PT], metaclass=ABCMeta):
         try:
             type_ = import_type(type_name)
         except (ImportError, AttributeError) as ex:
-            raise CannotImport(f"Cannot import type `{type_name}` from config '{name}'.") from ex
+            raise CannotImport(
+                f"Cannot import type `{type_name}` from config '{name}'."
+            ) from ex
 
         if self.abc and not issubclass(type_, self.abc):
             raise InvalidSubType(
@@ -295,7 +303,7 @@ class NamedPluginFactory(FactoryMixin[PT], metaclass=ABCMeta):
         else:
             try:
                 import_type(type_name)
-            except (ImportError, ValueError):
+            except (ImportError, ValueError, AttributeError):
                 messages.append(
                     checks.Error(
                         f"Unable to import type `{type_name}`.",
