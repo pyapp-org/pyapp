041111538b961c00024cdd3c0bd422f9a1259ffa
Tim Savage <tim@savage.company>
2019-07-19 17:34:59 +1000

    Added version_info block

--- pyapp/__init__.py
+++ pyapp/__init__.py
@@ -15,3 +15,4 @@ from .versioning import get_installed_version
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Tim Savage <tim@savage.company>"
+version_info = tuple(int(p) for p in __version__.split("."))
