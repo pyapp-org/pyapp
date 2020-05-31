a77ccb3d13eb44557289e8772c165854b74280ce
Tim Savage <tim@savage.company>
2020-05-31 18:45:34 +1000

    add python 3.8 to noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,6 @@ import nox
 from nox.sessions import Session
 
 
-@nox.session(python=("3.6", "3.7"))
+@nox.session(python=("3.6", "3.7", "3.8"))
 def tests(session: Session):
     session.run("python", "setup.py", "test")
