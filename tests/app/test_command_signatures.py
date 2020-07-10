17afff2911739cf0f484735ea4c73215a0a1fa9e
Tim Savage <tim@savage.company>
2020-07-11 02:14:00 +1000

    Built out test cases for Argument.from_parameter

--- tests/app/test_command_signatures.py
+++ tests/app/test_command_signatures.py
@@ -1,9 +1,31 @@
+from enum import Enum
+from enum import IntEnum
+from typing import Sequence
+from unittest import mock
+
 import pytest
 
-from unittest import mock
-from typing import Sequence
+from pyapp.app import KeyValueAction
+from pyapp.app.argument_actions import EnumName
+from pyapp.app.arguments import Arg
+from pyapp.app.arguments import CommandProxy
 
-from pyapp.app.arguments import Arg, CommandProxy
+
+def expected_args(*args):
+    """
+    Helper for applying expected add_argment calls to test functions
+    """
+
+    def _wrapper(func):
+        func.expected = list(args)
+        return func
+
+    return _wrapper
+
+
+class Colour:
+    Red = "red"
+    Green = "green"
 
 
 def func_backwards_compatible_1(opts):
@@ -14,55 +36,81 @@ def func_backwards_compatible_2(opts):
     pass
 
 
+@expected_args()
 def func_sample_1():
     pass
 
 
+@expected_args(mock.call("arg1", type=str, metavar="ARG1", dest="arg1"))
 def func_sample_2(arg1: str):
     pass
 
 
+@expected_args(mock.call("arg1", type=str, metavar="ARG1", dest="arg1", nargs="+"))
 def func_sample_3(arg1: Sequence[str]):
     pass
 
 
+@expected_args(mock.call("foo", type=int, metavar="FOO", dest="arg1"))
 def func_sample_4(arg1: int = Arg(name="foo")):
     pass
 
 
+@expected_args(
+    mock.call("arg1", type=str, metavar="ARG1", dest="arg1"),
+    mock.call("--arg2", type=str, metavar="ARG2", dest="arg2", default="foo"),
+)
 def func_sample_5(arg1: str, *, arg2: str = "foo"):
     pass
 
 
+@expected_args(
+    mock.call("arg1", type=str, metavar="ARG1", dest="arg1"),
+    mock.call("--arg2", action="store_true", metavar="ARG2", dest="arg2"),
+)
 def func_sample_6(arg1: str, *, arg2: bool):
     pass
 
 
-def func_sample_7(arg1: str, *, arg2: dict):
+@expected_args(mock.call("arg1", action=KeyValueAction, dest="arg1", nargs="+"),)
+def func_sample_7(arg1: dict):
     pass
 
 
+@expected_args(
+    mock.call("arg1", type=str, metavar="ARG1", dest="arg1"),
+    mock.call("--arg2", action=KeyValueAction, dest="arg2"),
+)
 def func_sample_8(arg1: str, *, arg2: dict):
     pass
 
 
+@expected_args(
+    mock.call("arg1", type=str, metavar="ARG1", dest="arg1"),
+    mock.call("--arg2", type=Colour, action=EnumName, dest="arg2", default=Colour.Red),
+)
+def func_sample_9(arg1: str, *, arg2: Colour = Colour.Red):
+    pass
+
+
 @pytest.mark.parametrize(
-    "handler, expected",
+    "handler",
     (
-        # (func_backwards_compatible_1, {}),
-        # (func_backwards_compatible_2, {}),
-        # (func_sample_1, {}),
-        (func_sample_2, [(("arg1",), {})]),
-        (func_sample_3, [(("arg1",), {})]),
-        (func_sample_4, [(("arg1",), {})]),
-        (func_sample_5, [(("arg1",), {})]),
-        (func_sample_6, [(("arg1",), {})]),
-        (func_sample_7, [(("arg1",), {})]),
-        (func_sample_8, [(("arg1",), {})]),
+        # func_backwards_compatible_1,
+        # func_backwards_compatible_2,
+        func_sample_1,
+        func_sample_2,
+        func_sample_3,
+        func_sample_4,
+        func_sample_5,
+        func_sample_6,
+        func_sample_7,
+        func_sample_8,
+        func_sample_9,
     ),
 )
-def test_from_parameter(handler, expected):
+def test_from_parameter(handler):
     mock_parser = mock.Mock()
     CommandProxy(handler, mock_parser)
 
-    mock_parser.add_argument.assert_called()
+    assert mock_parser.add_argument.mock_calls == handler.expected
