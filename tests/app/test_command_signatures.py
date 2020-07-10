5e2717e68ea7d6bea53bade8dadd69ea5a447b6c
Tim Savage <tim@savage.company>
2020-07-11 02:30:26 +1000

    Add more test cases

--- tests/app/test_command_signatures.py
+++ tests/app/test_command_signatures.py
@@ -1,5 +1,5 @@
+from argparse import FileType
 from enum import Enum
-from enum import IntEnum
 from typing import Sequence
 from unittest import mock
 
@@ -23,7 +23,7 @@ def expected_args(*args):
     return _wrapper
 
 
-class Colour:
+class Colour(Enum):
     Red = "red"
     Green = "green"
 
@@ -37,22 +37,22 @@ def func_backwards_compatible_2(opts):
 
 
 @expected_args()
-def func_sample_1():
+def func_sample_01():
     pass
 
 
 @expected_args(mock.call("arg1", type=str, metavar="ARG1", dest="arg1"))
-def func_sample_2(arg1: str):
+def func_sample_02(arg1: str):
     pass
 
 
 @expected_args(mock.call("arg1", type=str, metavar="ARG1", dest="arg1", nargs="+"))
-def func_sample_3(arg1: Sequence[str]):
+def func_sample_03(arg1: Sequence[str]):
     pass
 
 
 @expected_args(mock.call("foo", type=int, metavar="FOO", dest="arg1"))
-def func_sample_4(arg1: int = Arg(name="foo")):
+def func_sample_04(arg1: int = Arg(name="foo")):
     pass
 
 
@@ -60,7 +60,7 @@ def func_sample_4(arg1: int = Arg(name="foo")):
     mock.call("arg1", type=str, metavar="ARG1", dest="arg1"),
     mock.call("--arg2", type=str, metavar="ARG2", dest="arg2", default="foo"),
 )
-def func_sample_5(arg1: str, *, arg2: str = "foo"):
+def func_sample_05(arg1: str, *, arg2: str = "foo"):
     pass
 
 
@@ -68,12 +68,12 @@ def func_sample_5(arg1: str, *, arg2: str = "foo"):
     mock.call("arg1", type=str, metavar="ARG1", dest="arg1"),
     mock.call("--arg2", action="store_true", metavar="ARG2", dest="arg2"),
 )
-def func_sample_6(arg1: str, *, arg2: bool):
+def func_sample_06(arg1: str, *, arg2: bool):
     pass
 
 
 @expected_args(mock.call("arg1", action=KeyValueAction, dest="arg1", nargs="+"),)
-def func_sample_7(arg1: dict):
+def func_sample_07(arg1: dict):
     pass
 
 
@@ -81,7 +81,15 @@ def func_sample_7(arg1: dict):
     mock.call("arg1", type=str, metavar="ARG1", dest="arg1"),
     mock.call("--arg2", action=KeyValueAction, dest="arg2"),
 )
-def func_sample_8(arg1: str, *, arg2: dict):
+def func_sample_08(arg1: str, *, arg2: dict):
+    pass
+
+
+@expected_args(
+    mock.call("arg1", type=str, metavar="ARG1", dest="arg1"),
+    mock.call("--arg2", type=int, metavar="ARG2", dest="arg2", required=True),
+)
+def func_sample_09(arg1: str, *, arg2: int):
     pass
 
 
@@ -89,7 +97,12 @@ def func_sample_8(arg1: str, *, arg2: dict):
     mock.call("arg1", type=str, metavar="ARG1", dest="arg1"),
     mock.call("--arg2", type=Colour, action=EnumName, dest="arg2", default=Colour.Red),
 )
-def func_sample_9(arg1: str, *, arg2: Colour = Colour.Red):
+def func_sample_10(arg1: str, *, arg2: Colour = Colour.Red):
+    pass
+
+
+@expected_args(mock.call("arg1", type=FileType("w"), metavar="ARG1", dest="arg1"),)
+def func_sample_11(arg1: FileType("w")):
     pass
 
 
@@ -98,15 +111,17 @@ def func_sample_9(arg1: str, *, arg2: Colour = Colour.Red):
     (
         # func_backwards_compatible_1,
         # func_backwards_compatible_2,
-        func_sample_1,
-        func_sample_2,
-        func_sample_3,
-        func_sample_4,
-        func_sample_5,
-        func_sample_6,
-        func_sample_7,
-        func_sample_8,
-        func_sample_9,
+        func_sample_01,
+        func_sample_02,
+        func_sample_03,
+        func_sample_04,
+        func_sample_05,
+        func_sample_06,
+        func_sample_07,
+        func_sample_08,
+        func_sample_09,
+        func_sample_10,
+        func_sample_11,
     ),
 )
 def test_from_parameter(handler):
