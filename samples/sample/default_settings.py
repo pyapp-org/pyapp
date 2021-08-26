e2f3323cb380b7c5304e5df2d7a56bd2f363b1c2
Tim Savage <tim@savage.company>
2021-08-26 23:09:21 +1000

    Include a toml setting

--- samples/sample/default_settings.py
+++ samples/sample/default_settings.py
@@ -10,4 +10,22 @@ CHECK_LOCATIONS = ["sample.alt_checks"]
 INCLUDE_SETTINGS = [
     f"file://{here}/more-settings.json",
     f"file://{here}/more-settings.yaml",
+    f"file://{here}/more-settings.toml",
 ]
+
+LOGGING = {
+    "formatters": {
+        "default": {"format": "%(asctime)s | %(levelname)s | %(name)s | %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.FileHandler",
+            "formatter": "default",
+            "filename": "out.log",
+        },
+    },
+    "root": {
+        # 'level' : 'INFO',  # Set from command line arg parser.
+        "handlers": ["console"],
+    },
+}
