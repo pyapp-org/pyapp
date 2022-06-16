00d38bbd73fc6e02784c27cb3e83cc1ba1a68451
Tim Savage <tim@savage.company>
2022-06-16 15:41:35 +1000

    Update supported python versions in nox file

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 from nox.sessions import Session
 
 
-@nox.session(python=("3.6", "3.7", "3.8", "3.9"), reuse_venv=True)
+@nox.session(python=("3.8", "3.9", "3.10"), reuse_venv=True)
 def tests(session: Session):
     session.install("poetry")
     session.run("poetry", "install")
