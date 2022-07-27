1dae43c8a4deac6e0037babecd32808dec68ef65
Tim Savage <tim@savage.company>
2022-07-28 01:19:41 +1000

    Move init_logger and logging_formatter into the pyapp.logging module decoupling them explicitly from the pyapp.app package.
    Placeholders are left in place for compatability (to be removed in PyApp 5)

--- src/pyapp/__init__.py
+++ src/pyapp/__init__.py
@@ -6,11 +6,11 @@ pyApp - A python application framework
 *Let us handle the boring stuff!*
 
 """
-import logging
+import logging as _log
 
 from pyapp.versioning import get_installed_version
 
 # Configure a default null handler for logging.
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+_log.getLogger(__name__).addHandler(_log.NullHandler())
 
 __author__ = "Tim Savage <tim@savage.company>"
