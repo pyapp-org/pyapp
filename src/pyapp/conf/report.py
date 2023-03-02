0db9116b8354d8906099f12a1857dc8208a5a64e
Tim Savage <tim@savage.company>
2023-03-03 00:43:06 +1100

    Port conf report to Rich

--- src/pyapp/conf/report.py
+++ src/pyapp/conf/report.py
@@ -5,75 +5,43 @@ Conf Report
 Generates settings reports
 
 """
-import pprint
-import sys
-from typing import TextIO
+from rich.console import Console
+from rich.pretty import Pretty
+from rich.table import Table
 
-from colorama import Fore
-from colorama import Style
 from pyapp import conf
-from pyapp.utils import wrap_text
 
 
 class SettingsReport:
-    """
-    Report of all settings in use.
-    """
-
-    width = 96
+    """Report of all settings in use."""
 
     def __init__(
         self,
+        console: Console,
         verbose: bool = False,
-        no_color: bool = False,
-        f_out: TextIO = sys.stdout,
         settings: str = None,
     ):
-        """
-        Initialise check report
+        """Initialise check report.
 
+        :param console: Console output
         :param verbose: Enable verbose output
-        :param no_color: Disable colourised output
-        :param f_out: File to output report to; default is ``stdout``
         :param settings: Settings to produce a report of.
-
         """
+        self.console = console
         self.verbose = verbose
-        self.f_out = f_out
-        self.no_color = no_color
         self.settings = settings or conf.settings
 
-        # Generate templates
-        if self.no_color:
-            self.basic_template = "{key:20} : {ppsetting}\n"
+    def output(self):
+        """Output a result to output file."""
+        table = Table(title="Settings")
+        table.add_column("Setting", style="yellow")
+        table.add_column("Value")
 
-        else:
-            self.basic_template = (
-                Fore.YELLOW
-                + "{key:20} : "
-                + Fore.CYAN
-                + "{ppsetting}"
-                + Style.RESET_ALL
-                + "\n"
-            )
+        for key, value in self.settings.items():
+            table.add_row(key, Pretty(value))
 
-    def output_result(self, key: str, setting):
-        """
-        Output a result to output file.
-        """
-        format_args = dict(
-            key=key,
-            setting=setting,
-            ppsetting=wrap_text(
-                pprint.pformat(setting, 2), width=self.width, indent=25
-            ).strip(),
-        )
-
-        self.f_out.write(self.basic_template.format(**format_args))
+        return table
 
     def run(self):
-        """
-        Run the report
-        """
-        for key, value in self.settings.items():
-            self.output_result(key, value)
+        """Run the report."""
+        self.console.print(self.output())
