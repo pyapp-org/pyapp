190c8bcfc9af1553ba471c414fcae00e22a15642
Tim Savage <tim@savage.company>
2024-10-11 01:55:55 +1100

    Add function to get a list of all settings in a module

--- src/pyapp/testing/__init__.py
+++ src/pyapp/testing/__init__.py
@@ -1,4 +1,26 @@
 """Helper methods for testing.
 
- Includes a plugin for Pytest.
- """
+Includes a plugin for Pytest.
+"""
+
+from types import ModuleType
+
+from ..conf.loaders import settings_iterator
+
+
+def settings_in_module(
+    *modules: ModuleType,
+    exclude: list[str] = ("INCLUDE",),
+):
+    """Generate a list of settings defined in a module (or modules).
+
+    Used for ensuring that a second settings module only contains specified settings.
+    """
+    settings = set()
+
+    for mod in modules:
+        settings.update(
+            name for name, _ in settings_iterator(mod) if name not in exclude
+        )
+
+    return settings
