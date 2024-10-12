20ab9fa20ded19104f00960c2b765a7451fe7a13
Tim Savage <tim@savage.company>
2024-10-12 20:58:27 +1100

    Added test_injection_fixture

--- tests/unit/testing/test_pytest_plugin.py
+++ tests/unit/testing/test_pytest_plugin.py
@@ -1,3 +1,5 @@
+import abc
+
 from pyapp import feature_flags
 
 
@@ -11,6 +13,15 @@ def test_feature_flags_fixture(patch_feature_flags):
     patch_feature_flags["foo"] = True
 
     assert feature_flags.get("foo") is True
+
+
+def test_injection_fixture(patch_injection, pyapp_factory_registry):
+    class MyType(abc.ABC):  # noqa B024
+        pass
+
+    mock = patch_injection.mock_type(MyType, allow_missing=True)
+
+    assert pyapp_factory_registry[MyType]() is mock
 
 
 def test_check_registry(pytester):
