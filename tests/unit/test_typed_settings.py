959ec7e1de3e4c18d885c7438d031d0f8cf14962
Tim Savage <tim@savage.company>
2024-11-27 17:16:46 +1100

    Add tests for prefix issue

--- tests/unit/test_typed_settings.py
+++ tests/unit/test_typed_settings.py
@@ -1,4 +1,4 @@
-from ..settings import MySettings
+from ..settings import MySettings, MyPrefixedSettings
 
 
 def test_access_settings():
@@ -9,3 +9,10 @@ def test_access_settings__where_setting_modified(patch_settings):
     patch_settings.UPPER_VALUE = "bar"
 
     assert MySettings.UPPER_VALUE == "bar"
+
+
+def test_access_settings__where_settings_has_a_prefix(settings):
+    assert settings.FOO_SETTING_1 == "my-prefixed-setting"
+    assert MyPrefixedSettings.SETTING_1 == "my-prefixed-setting"
+    assert MySettings.SETTING_1 == 1
+
